f7b399e7539decc093da795c056f789bc09f8f2c
Pauliih <pau.campos89@gmail.com>
2018-03-31 15:46:08 -0300

    Controlated components (most recomendated for forms)

--- src/sections/forms.js
+++ src/sections/forms.js
@@ -1,18 +1,26 @@
 import React, {Component} from 'react';
 
 export default class Form extends Component {
+  // 1. Agrego un state inicial
+  constructor() {
+    super();
+    this.state = {
+      inputName: '',
+      inputTwitter: '@',
+      inputTerms: true
+    };
+  }
   // Creo el método
   handleSubmit = (event) => {
     event.preventDefault(); // para evitar que el botóm haga submit
-    const name = this.inputName.value;
-    const email = document.getElementById('twitter').value;
-    console.log({name, 
-      email });
+    console.log(this.state);
   }
 
   handleChange = (event) => {
     console.log('handleChange');
     console.log(event.target.checked);
+    // 5 modificamos el inputTerms con el nuevo valor 
+    this.setState({inputTerms: event.target.checked});
   }
 
   render() {
@@ -25,9 +33,13 @@ export default class Form extends Component {
             <input
               id='name'
               name='userName'
+              // 3 agregamos onChange para  cambiar su estado manualmente
+              onChange={event => this.setState({inputName: event.target.value})} 
               placeholder='Introduce el nombre'
               // Añado el atributo ref al input para recuperar el valor
-              ref={inputElement => this.inputName = inputElement} />
+              ref={inputElement => this.inputName = inputElement} 
+              // 2 usamos el state inicial para que cada input muestre el valor
+              value={this.state.inputName}/>
           </p>
 
           <p>
@@ -35,12 +47,19 @@ export default class Form extends Component {
             <input
               id='twitter'
               name='twitterAccount'
-              placeholder='Introduce tu Twitter' />
+              // 3 agregamos onChange para  cambiar su estado manualmente
+              onChange={event => this.setState({ inputTwitter: event.target.value })} 
+              placeholder='Introduce tu Twitter'
+              // 2 usamos el state inicial para que cada input muestre el valor
+              value={this.state.inputTwitter} />
           </p>  
 
           <p>
             <label>
-              <input onChange={this.handleChange}
+              <input 
+                // 2 usamos el state inicial para que cada input muestre el valor
+                checked={this.state.inputTerms}
+                onChange={this.handleChange}
                 type='checkbox' />
                 Accepted terms
             </label>
