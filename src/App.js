d95d014556c0c4a86dece1ac0ee301a492d547ce
Pauliih <pau.campos89@gmail.com>
2018-03-31 13:53:34 -0300

    event soported I

--- src/App.js
+++ src/App.js
@@ -13,6 +13,23 @@ Title.defaultProps = {
 };
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { mouseX: 0, 
+      mouseY: 0 };
+    // 1) Enlazamos el método que controla al evento  
+    // Utilizando el método bind, que devuelve el mismo método pero en el contexto correcto.
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+  }
+  // Creo el controlador del evento
+  handleMouseMove(event) {
+    // extraemos del evento la posición del mouse relativa al área del contenedor
+    const { clientX, clientY} = event;
+    // actualizamos el state del componente con setState
+    this.setState({mouseX: clientX, 
+      mouseY: clientY});
+  }
+
   handleClick(event) {
     // evento sintético, hace que sea compatible con todos los navegadores
     // soportados por react
@@ -27,6 +44,15 @@ class App extends Component {
         <Title />
         {/* Evento onClick del botón */}
         <button onClick={this.handleClick}>Hi There!</button>
+        <div 
+          // Agrego la props al elemento div usando como funcion el handleMouseMove que habíamos creado
+          onMouseMove={this.handleMouseMove}
+          style={{border: '1px solid #000', 
+            margin: 10, 
+            padding: 10}}>
+          {/* Mostramos el state actual */}
+          <p>{this.state.mouseX}, {this.state.mouseY}</p>
+        </div>
       </div>
     );
   }
